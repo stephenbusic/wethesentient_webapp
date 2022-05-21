9a71701fb9e4f2bcd8385d5256a92efb3aadc901
scuub <aaahhhghosts@gmail.com>
2022-05-20 23:43:00 -0400

    improved email logging.

--- common/util/send_email.py
+++ common/util/send_email.py
@@ -8,4 +8,9 @@ def send_email(email, subject, html_content, text_content):
 
     # Send email with subject as both html and plain text
     logging.info("Sending email to {0}...".format(email))
-    return bool(send_mail(subject, text_content, None, [email], html_message=html_content, fail_silently=False))
+    sent = bool(send_mail(subject, text_content, None, [email], html_message=html_content, fail_silently=False))
+    if sent:
+        logging.info("[SUCCESS] email sent!")
+    else:
+        logging.info("[FAILED] not sent!")
+    return sent
